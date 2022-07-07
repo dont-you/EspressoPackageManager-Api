a0498f03202e0a136066af046cb51add14559ed4
who <who@192>
2022-07-07 18:24:24 +0300

    Feature: static function connection to db

--- src/main/java/com/espresso/api/dbhandlers/DataBaseConnector.java
+++ src/main/java/com/espresso/api/dbhandlers/DataBaseConnector.java
@@ -8,4 +8,15 @@ public final class DataBaseConnector {
     static final private String serverAdress = "localhost:3306";
     static final private String dataBaseName = "PackageManager";
     static final private String dataBaseUserName = "root";
+
+    static{
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://"+serverAdress+"/"+dataBaseName,dataBaseUserName,"1337");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("==================================================================================================================================");
+            System.out.println(e.getMessage());
+            System.out.println("==================================================================================================================================");
+        }
+    }
 }
