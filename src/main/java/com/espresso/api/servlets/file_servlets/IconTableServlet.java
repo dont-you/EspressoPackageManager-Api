7632c689ecb6c2ceb07fdc809dcd15009a0fcdb8
who <who@192>
2022-07-07 16:12:31 +0300

    Feature: clear servlet functional
    in the future, new functional will be added

--- src/main/java/com/espresso/api/servlets/file_servlets/IconTableServlet.java
+++ src/main/java/com/espresso/api/servlets/file_servlets/IconTableServlet.java
@@ -9,11 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.espresso.api.servlets.MainServlet;
-import com.espresso.api.tables.IconTable;
-
-import org.json.JSONObject;
-
 @WebServlet(urlPatterns = {"/icons","/icons/*"})
 @MultipartConfig(maxFileSize = 262144,location = "/home/who/Projects/PackageManager/DataBaseFiles/icons")
 public class IconTableServlet extends HttpServlet
@@ -31,27 +26,18 @@ public class IconTableServlet extends HttpServlet
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp){
-        try {
-            MainServlet.doGet(req, resp, new IconTable());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        JSONObject jsonBody = FileManager.writeFileFromeRequest(req, "image","/home/who/Projects/PackageManager/DataBaseFiles/icons/");
-        MainServlet.doPost(req, resp, new IconTable(),jsonBody.toString());
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // MainServlet.doPut(req, resp, new IconTable());
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        MainServlet.doDelete(req, resp, new IconTable());
     }
 
     @Override
--- src/main/java/com/espresso/api/servlets/json_servlets/ApplicationTableServlet.java
+++ src/main/java/com/espresso/api/servlets/json_servlets/ApplicationTableServlet.java
@@ -1,7 +1,6 @@
 package com.espresso.api.servlets.json_servlets;
 
 import java.io.IOException;
-import java.util.stream.Collectors;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
@@ -9,9 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.espresso.api.servlets.MainServlet;
-import com.espresso.api.tables.ApplicationTable;
 
 @WebServlet(urlPatterns = {"/applications","/applications*"})
 public class ApplicationTableServlet extends HttpServlet
@@ -30,28 +26,18 @@ public class ApplicationTableServlet extends HttpServlet
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp){
-        try {
-            MainServlet.doGet(req, resp, new ApplicationTable());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String jsonBody = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-        MainServlet.doPost(req, resp, new ApplicationTable(),jsonBody);
     }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String jsonBody = req.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
-        MainServlet.doPut(req, resp, new ApplicationTable(),jsonBody);
     }
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        MainServlet.doDelete(req, resp, new ApplicationTable());
     }
 
     @Override
