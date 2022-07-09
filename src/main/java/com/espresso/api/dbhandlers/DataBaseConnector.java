8607a17e99a2e07a83826f597952099f9b2b1534
who <who@192>
2022-07-09 13:08:26 +0300

    Refactor: substitute static connection to non-static

--- src/main/java/com/espresso/api/dbhandlers/DataBaseConnector.java
+++ src/main/java/com/espresso/api/dbhandlers/DataBaseConnector.java
@@ -3,13 +3,13 @@ package com.espresso.api.dbhandlers;
 import java.sql.*;
 
 public final class DataBaseConnector {
-    static public Connection connection = null;
+    public Connection connection = null;
 
     static final private String serverAdress = "localhost:3306";
     static final private String dataBaseName = "PackageManager";
     static final private String dataBaseUserName = "root";
 
-    static{
+    DataBaseConnector(){
         try {
             Class.forName("com.mysql.jdbc.Driver");
             connection = DriverManager.getConnection("jdbc:mysql://"+serverAdress+"/"+dataBaseName,dataBaseUserName,"1337");
