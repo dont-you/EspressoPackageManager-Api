5fdda20cff19de628a697b6742c3fea2b9717f55
who <who@192>
2022-07-23 14:56:21 +0300

    Feature: add function retrivePrimaryKeyFromResultSet

--- src/main/java/com/espresso/api/tables/IconTable.java
+++ src/main/java/com/espresso/api/tables/IconTable.java
@@ -1,5 +1,8 @@
 package com.espresso.api.tables;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class IconTable extends Table{
     public Integer id = null;
     public String imagePath = null;
@@ -28,5 +31,14 @@ public class IconTable extends Table{
     @Override
     public void dataValidation() {
 
+    }
+
+    @Override
+    public String retrivePrimaryKeyFromResultSet(ResultSet generatedKeys) throws SQLException {
+        String pk = null;
+        while(generatedKeys.next()){
+            pk = generatedKeys.getLong(1) + "";
+        }
+        return pk;
     }
 }
