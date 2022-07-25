9550d3d01864238350897bb1f512895c8882f3cd
who <who@192>
2022-07-25 13:11:58 +0300

    Revert "Feature: add function retrivePrimaryKeyFromResultSet"
    This reverts commit a612d74f3104b389f6741b5f97a35f00787f649d. All
    primary key now have long type

--- src/main/java/com/espresso/api/tables/LibraryHasApplicationTable.java
+++ src/main/java/com/espresso/api/tables/LibraryHasApplicationTable.java
@@ -1,8 +1,5 @@
 package com.espresso.api.tables;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 public class LibraryHasApplicationTable extends Table{
     public Integer library_id=null;
     public Integer application_id=null;
@@ -31,8 +28,4 @@ public class LibraryHasApplicationTable extends Table{
 
     }
 
-    @Override
-    public String retrivePrimaryKeyFromResultSet(ResultSet generatedKeys) throws SQLException {
-        return null;
-    }
 }
